5718175ce5f5de53076d106c1cd56d186a8d25ec
emtseng <etseng42@gmail.com>
2017-10-12 20:51:41 -0400

    scaffolds setGoals component

--- src/main/resources/assets/jsx/components/SetGoals.js
+++ src/main/resources/assets/jsx/components/SetGoals.js
@@ -1,7 +1,61 @@
 import React, { Component } from 'react'
 
 class SetGoals extends Component {
-
+  constructor() {
+    super()
+    this.state = {
+      cookGoals: 0,
+      eatGoals: 0
+    }
+  }
+  submitGoals(evt) {
+    evt.preventDefault()
+    evt.stopPropagation()
+    //ajax request with goals
+  }
+  handleFormChange(evt) {
+    evt.preventDefault()
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+  render() {
+    return (
+      <div id="setGoals">
+        Set your weekly goals.
+        <div id="setGoalsForm">
+          <form>
+            Cook:
+            <input
+              name="cookGoals"
+              id="setGoalsCook"
+              type="number"
+              value={this.state.cookGoals}
+              min="0"
+              max="21"
+              onChange={this.handleFormChange}
+            />
+            Eat:
+            <input
+              name="eatGoals"
+              id="setGoalsEat"
+              type="number"
+              value={this.state.eatGoals}
+              min="0"
+              max="21"
+              onChange={this.handleFormChange}
+            />
+          </form>
+        </div>
+        <div id="setGoalsBtns">
+          <button
+            onClick={this.submitGoals}
+          >
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default SetGoals
