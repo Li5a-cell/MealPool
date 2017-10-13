6f8fce66c8dd76fe824785338fa23145b48c6f8f
mwhite494 <mwhite494@gmail.com>
2017-10-13 14:49:10 -0400

    Most recent frontend

--- src/main/resources/assets/jsx/components/ScheduleCook.js
+++ src/main/resources/assets/jsx/components/ScheduleCook.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import MealRow from './MealRow.js'
 
@@ -21,54 +22,36 @@ class ScheduleCook extends Component {
     evt.stopPropagation()
     //ajax request for that day's meals
   }
+  submitRecipe(evt) {
+    evt.preventDefault()
+      evt.stopPropagation()
+      console.log(this.refs.recipe.value)
+      //const body = {}
+      //axios.post('/api/schedule', )
+  }
   render() {
     return (
-      <div id="ScheduleCook">
-        <div id="weekView">
-          <button
-            onClick={this.selectDay}
-            className="weekBtn">
-            Sunday
-          </button>
-          <button
-            onClick={this.selectDay}
-            className="weekBtn">
-            Monday
-          </button>
-          <button
-            onClick={this.selectDay}
-            className="weekBtn">
-            Tuesday
-          </button>
-          <button
-            onClick={this.selectDay}
-            className="weekBtn">
-            Wednesday
-          </button>
-          <button
-            onClick={this.selectDay}
-            className="weekBtn">
-            Thursday
-          </button>
-          <button
-            onClick={this.selectDay}
-            className="weekBtn">
-            Friday
-          </button>
-          <button
-            onClick={this.selectDay}
-            className="weekBtn">
-            Saturday
-          </button>
+        <div id="ScheduleCook">
+          <div id="cookForm">
+            <form onSubmit={this.submitRecipe}>
+              Date:
+              <select ref="cookDate">
+                <option value="Sunday">Sunday (10-15-2017)</option>
+                <option value="Monday">Monday (10-16-2017)</option>
+                <option value="Tuesday">Tuesday (10-17-2017)</option>
+                <option value="Wednesday">Wednesday (10-18-2017)</option>
+                <option value="Thursday">Thursday (10-19-2017)</option>
+                <option value="Friday">Friday (10-20-2017)</option>
+                <option value="Saturday">Saturday (10-21-2017)</option>
+              </select><br/>
+              Recipe: <input ref="recipe" type="text" placeholder="Enter Recipe Name" ></input><br/>
+              Description: <input ref="description" type="text" placeholder="Enter a Description" ></input><br/>
+              Max Orders: <input ref="maxOrders" type="number" ></input><br/>
+              Price: <input ref="price" type="number" ></input><br/>
+              <input type="submit" value="Submit Recipe" ></input>
+            </form>
+          </div>
         </div>
-        <div id="meals">
-        {
-          this.state.meals.length > 0 ?
-          this.state.meals.map(meal => <MealRow meal={meal} />) :
-          null
-        }
-        </div>
-      </div>
     )
   }
 }
